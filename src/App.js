45fa4f5951c8934300a42fabb9df0206146a6166
Asraf Ali <asraf.bookaand@gmail.com>
2022-05-23 11:19:19 +0600

    install react router and create navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Shared/Navbar";
 
 function App() {
   return (
     <div>
-     
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
